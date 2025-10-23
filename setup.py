52bdfc42ca185ceb4389e27d2cff596cd3da59f3
Iury O. G. Figueiredo <last.src@gmail.com>
2025-10-22 23:39:51 -0300

    Fixing setup version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name="vyirc",
-      version="1.2.0",
+      version="1.2.1",
       description="An irc plugin for vy.",
       py_modules=["vyirc"],
       author="Iury O. G. Figueiredo",
