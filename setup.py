84abaed5731da93dcb924f342d5e864fab010eaf
Iury O. G. Figueiredo <last.src@gmail.com>
2025-10-26 14:13:42 -0300

    Changing version scheme.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name="vyirc",
-      version="1.2.1",
+      version="1.0.0.0",
       description="An irc plugin for vy.",
       py_modules=["vyirc"],
       author="Iury O. G. Figueiredo",
